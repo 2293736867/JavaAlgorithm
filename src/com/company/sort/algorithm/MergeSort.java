8ab6ac7d79d30092a1d600505f9e8db2a29ff706
2293736867 <2293736867@qq.com>
2021-12-08 19:34:35 +0800

    Heap sort adjust array moving

--- src/com/company/sort/algorithm/MergeSort.java
+++ src/com/company/sort/algorithm/MergeSort.java
@@ -6,54 +6,54 @@ import java.util.Arrays;
 
 public class MergeSort extends Sort {
 
-    public MergeSort(){
+    public MergeSort() {
 
     }
 
-    public MergeSort(int []arr){
+    public MergeSort(int[] arr) {
         super(arr);
     }
 
     @Override
     protected int[] doSort() {
-        int[] l = divide(0,n/2);
-        int[] r = divide(n/2,n);
-        return merge(l,r);
+        int[] l = divide(0, n / 2);
+        int[] r = divide(n / 2, n);
+        return merge(l, r);
     }
 
-    private int[] merge(int []l,int []r){
+    private int[] merge(int[] l, int[] r) {
         int lLen = l.length;
         int rLen = r.length;
-        int [] result = new int[lLen+rLen];
+        int[] result = new int[lLen + rLen];
         int i = 0;
         int j = 0;
         int sIndex = 0;
-        while(i < lLen && j < rLen){
-            if(l[i] > r[j]){
+        while (i < lLen && j < rLen) {
+            if (l[i] > r[j]) {
                 result[sIndex++] = r[j];
                 ++j;
-            }else{
+            } else {
                 result[sIndex++] = l[i];
                 ++i;
             }
         }
-        while(i < lLen){
-            result[sIndex++] = l[i++];
+        if (i < lLen) {
+            System.arraycopy(l, i, result, sIndex, lLen - i);
         }
-        while(j < rLen){
-            result[sIndex++] = r[j++];
+        if (j < rLen) {
+            System.arraycopy(r, j, result, sIndex, rLen - j);
         }
         return result;
     }
 
-    private int[] divide(int l,int r){
-        if(r - l > 1){
-            int m = (l+r)/2;
-            int [] lNum = divide(l,m);
-            int [] rNum = divide(m,r);
-            return merge(lNum,rNum);
-        }else{
-            return Arrays.copyOfRange(arr,l,r);
+    private int[] divide(int l, int r) {
+        if (r - l > 1) {
+            int m = (l + r) / 2;
+            int[] lNum = divide(l, m);
+            int[] rNum = divide(m, r);
+            return merge(lNum, rNum);
+        } else {
+            return Arrays.copyOfRange(arr, l, r);
         }
     }
 }
